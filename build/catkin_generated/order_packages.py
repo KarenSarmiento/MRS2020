453145e9a9ac53b81c2c56a137323b457ad23f2b
drsphelps <drsphelps@gmail.com>
2020-02-13 21:15:11 +0000

    init

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mrs/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mrs/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/mrs/catkin_ws/devel;/opt/ros/kinetic" != "" else []
